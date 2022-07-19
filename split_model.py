b479305ebdb027bd37ce99a4996836362c5955a1
nbovee <12849851+nbovee@users.noreply.github.com>
2022-07-19 00:18:35 -0400

    working split model with internal state optimized

--- split_model.py
+++ split_model.py
@@ -2,27 +2,47 @@ from distutils.log import error
 import torch
 import torch.nn as nn
 import copy
+import cv2
+
 from models.experimental import attempt_load
 from utils.torch_utils import select_device
 from torchinfo import summary
 import models.yolo as yolo
 from models.yolo import Detect, IDetect, IAuxDetect, IBin
 import sys
+from utils.plots import plot_one_box
+
 import numpy as np
 from PIL import Image
 import thop
-from utils.torch_utils import time_synchronized
+from numpy import random
+import yaml
+from utils.torch_utils import time_synchronized, copy_attr
+from utils.datasets import LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 
 class SplitModel(yolo.Model):
-    def __init__(self, cfg='cfg/training/yolov7.yaml', ch=3, nc=None, anchors=None, side = "edge", start_layer = 0, layers = 105, end_layer = 105 ) -> None:
-        self.start_layer = start_layer
-        self.end_layer = end_layer
-        self.layers = layers
-        self.side = side
+    def __init__(self, pretrained = None, cfg='cfg/deploy/yolov7.yaml', ch=3, nc=None, anchors=None, num_layers = 106 ) -> None:
+        with open('forward_optimization.yaml', 'r') as file:
+            self.forward_optimization = yaml.load(file, Loader=yaml.FullLoader)
+        self.layers = num_layers
         super(SplitModel, self).__init__(cfg, ch, nc, anchors)
+        if pretrained:
+            self.load_weights_of_model(pretrained)
 
-    def forward(self, x, augment=False, profile=False, inference = False, st = 0, en = 105, y_new = None):
-        augment = False #lock these out for now, not prepared for split network 
+    def load_weights_of_model(self, input_model):
+        copy_attr(input_model, self, include=('yaml', 'nc', 'names', 'stride', 'save'), exclude=())  # copy attributes
+        self.fuse() # unify conv+bn and removes training weights
+        self.model.load_state_dict(input_model.model.state_dict())
+
+    def autoshape(self):
+        raise Exception("Not implemented.")
+
+    def forward(self, x, augment=False, profile=False, y_from_edge = None, start_layer = None, end_layer = None, split = None):
+        st = 0 if start_layer is None else start_layer # default entry
+        en = self.layers if end_layer is None else end_layer # default exit
+        augment = False #lock this out for now, not prepared for split network 
         if augment:
             img_size = x.shape[-2:]  # height, width
             s = [1, 0.83, 0.67]  # scales
@@ -40,19 +60,23 @@ class SplitModel(yolo.Model):
                 y.append(yi)
             return torch.cat(y, 1), None  # augmented inference, train
         else:
-            return self.forward_once(x, profile, inference = inference, st = st, en = en, y_new = y_new)  # single-scale inference, train
+            return self.forward_once(x, st, en, y_from_edge, profile, split)  # single-scale inference, train
 
-    def forward_once(self, x, profile=False, st = 0, en = 105, inference = False, y_new = None):
+    def forward_once(self, x, st, en, y_from_edge, profile=False, split = None):
         y, dt = [], []  # outputs
-        if y_new:
-            y = y_new
+        y = y_from_edge if y_from_edge is not None else []
         enter_val = st
         exit_val = en
         counter = 0
         for m in self.model:
-            if inference and exit_val < counter:
+            if exit_val < counter:
                 break # exit early
-            if not inference or enter_val <= counter:
+            if enter_val <= counter:
+                needed = set()
+                for i in self.forward_optimization[counter-1:]:
+                    if i is not None:
+                        for e in i:
+                            needed.add(e)
                 if m.f != -1:  # if not from previous layer
                     x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
                 if not hasattr(self, 'traced'):
@@ -73,51 +97,16 @@ class SplitModel(yolo.Model):
                     dt.append((time_synchronized() - t) * 100)
                     print('%10.1f%10.0f%10.1fms %-40s' % (o, m.np, dt[-1], m.type))
                 x = m(x)  # run
-                
                 y.append(x if m.i in self.save else None)  # save output
+                # reduce size of y list for transfer to server                
+                for i, y_i in enumerate(y):
+                    if i not in needed:
+                        y[i] = None
             counter += 1
 
         if profile:
             print('%.1fms total' % sum(dt))
-        if inference:
-            return x, y # y must be available for reference if split, but we do not want to interfere with the source of yolov7
+        if split:
+            return x, y # y must be available for reference if split, but we do not want to interfere with normal operation
         else:
             return x
-
-
-
-weights = 'yolov7.pt'
-device = select_device('0')
-edge_model = SplitModel(end_layer = 50)
-cloud_model = SplitModel(start_layer = 51)
-edge_model.to(device)
-cloud_model.to(device)
-edge_model.eval()
-cloud_model.eval()
-for i in range(10):
-    edge_model(torch.zeros(1, 3, 640, 640).to(device).type_as(next(edge_model.parameters())))  # run warmup blind
-# x, y = edge_model(torch.zeros(1, 3, 640, 640).to(device).type_as(next(edge_model.parameters())), inference = True)  # now profile
-# print(time_synchronized() - t)
-# torch.save(y, "yolov7_y_rand.pt")
-# print(torch.tensor(y, dtype=torch.float64, device=device))
-# y_mem = torch.load("yolov7_y_rand.pt")
-d = 0
-# summary(edge_model, input_size=(1, 3, 640, 640), depth = d, inference = True)
-# summary(edge_model, input_size=(1, 3, 640, 640), depth = d, en = 50, inference = True)
-# summary(cloud_model, input_size=(1, 1024, 20, 20), depth = d, st = 51, inference = True, y_new = y)
-img = '../coco/images/test2017/000000000001.jpg'
-t = time_synchronized()
-input = torch.zeros(1, 3, 640, 640).to(device).type_as(next(edge_model.parameters()))
-# input = torch.from_numpy(img).to(device)
-t2 = time_synchronized()
-for i in range(100):
-    x2, _ = cloud_model(input, inference = True)
-t3 = time_synchronized()
-print(f"{(t3-t2)/100:.04f}")
-# for i in range(106):
-#     ts = time_synchronized()
-#     x, y = edge_model(input, en = i, inference = True)
-#     t3 = time_synchronized()
-#     x2, _ = cloud_model(x, y_new = y, st = i + 1, inference = True)
-#     t4 = time_synchronized()
-#     print(f"{i}\t{sys.getsizeof(x)}\t{sys.getsizeof(y)}\t{t3-ts:.04f}\t{t4-t3:.04f}")
